69509f939381359294c832a92ced02b7f2f082bd
kai00588 <61916927+kai00588@users.noreply.github.com>
2024-01-16 12:21:03 +0000

    dasdas

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,10 +1,11 @@
 <script>
     //@ts-nocheck
     export let anime;
+    import {useLazyImage as lazyImage} from "svelte-lazy-image";
 </script>
 
 <a href="/content/anime/{anime.episodeId.split('-episode-')[0]}" class="relative w-[100%] border border-[black] pb-2 hover:curosor-pointer">
-    <img class="object-cover h-[92%]" src="{anime.animeImg}" alt="{anime.animeTitle}" srcset="">
+    <img class="object-cover h-[92%]" use:lazyImage data-src="{anime.animeImg}" alt="{anime.animeTitle}" srcset="">
     <div class="w-full h-10">
         <h2 class="pl-2 text-white font-bold truncate">{anime.animeTitle}</h2>
     </div>
