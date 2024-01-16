6da0a7ee2b0a1a337e7669e794644e74107814af
kai00588 <61916927+kai00588@users.noreply.github.com>
2024-01-16 14:35:27 +0000

    New Commit

---
+++ src/components/RightBar.svelte
@@ -0,0 +1,28 @@
+<script>
+    //@ts-nocheck
+    import { api_end } from "$lib/store";
+    import axios from "axios";
+    import { onMount } from "svelte";
+    import Card from "./Card.svelte";
+
+
+    $: animes = []
+    onMount(async ()=>{
+        const res = await axios.get(`${$api_end}/top-airing`)
+        animes = res.data;
+    })
+</script>
+
+<div class="w-full h-fit border border-slate-900">
+    <div class="bg-orange-300 w-full h-6">
+        <p class="uppercase pl-2">popular</p>
+    </div>
+    <div class="grid grid-cols-2 md:grid-cols-2 gap-1">
+        <h2>1</h2>
+
+        
+        <!-- {#each animes as anime}
+            <Card anime={anime}/>
+        {/each} -->
+    </div>
+</div>
