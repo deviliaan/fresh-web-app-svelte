46e2d712a7431a8f02a5388a81ef395ad742843a
kai00588 <61916927+kai00588@users.noreply.github.com>
2024-01-16 06:27:35 +0000

    v1.0.1

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<nav class="bg-slate-800 h-16 w-full">
+    <div class="flex justify-center h-full align-middle">
+        <img src="https://picsum.photos/200" alt="Logo">
+    </div>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script>
     import '../app.css';
+    import Navbar from '../components/Navbar.svelte';
     
 </script>
 
 <section>
     <div class="">
+        <Navbar />
         <slot />
     </div>
 </section>
