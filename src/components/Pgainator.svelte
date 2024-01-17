ff17186913764aedcf6a574bbcb1aa8402b992ea
kai00588 <61916927+kai00588@users.noreply.github.com>
2024-01-17 13:22:35 +0000

    ui changes

--- src/components/Pgainator.svelte
+++ src/components/Pgainator.svelte
@@ -7,13 +7,13 @@
 <div class="w-full flex justify-center items-center gap-1 pt-2 mb-4 h-16">
     <div class="join">
         {#if page != 1 && page != 2}
-            <a href="#" class="join-item btn btn-md">{page - 2}</a>
+            <a href="/?page={page - 2}&type={type}" class="join-item btn btn-md">{page - 2}</a>
         {/if}
         {#if page != 1}
-            <a href="#" class="join-item btn btn-md">{page - 1}</a>
+            <a href="/?page={page - 1}&type={type}" class="join-item btn btn-md">{page - 1}</a>
         {/if}
         <a href="#" class="join-item btn btn-md btn-active">{page}</a>
-        <a href="#" class="join-item btn btn-md">{page + 1}</a>
-        <a href="#" class="join-item btn btn-md">{page + 2}</a>
+        <a href="/?page={page + 1}&type={type}" class="join-item btn btn-md">{page + 1}</a>
+        <a href="/?page={page + 2}&type={type}" class="join-item btn btn-md">{page + 2}</a>
     </div>
 </div>
