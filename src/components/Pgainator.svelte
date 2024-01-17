c109a630d863481df1dd9672e77eea3e0f594419
kai00588 <61916927+kai00588@users.noreply.github.com>
2024-01-17 13:15:20 +0000

    pagination upgraded

--- src/components/Pgainator.svelte
+++ src/components/Pgainator.svelte
@@ -5,13 +5,15 @@
 </script>
 
 <div class="w-full flex justify-center items-center gap-1 pt-2 mb-4 h-16">
-    {#if page > 1 && page > 2}
-        <a data-sveltekit-preload-data="tap" href="/?page={page - 2}&type={type}" class="bg-orange-300 font-bold rounded-lg px-4 hover:cursor-pointer">{page - 2}</a>
-    {/if}
-    {#if page > 1}
-        <a data-sveltekit-preload-data="tap" href="/?page={page - 1}&type={type}" class="bg-orange-300 font-bold rounded-lg px-4 hover:cursor-pointer">{page - 1}</a>
-    {/if}
-    <a  href="/?page={page}" class="bg-orange-100 font-bold rounded-lg px-4 hover:cursor-pointer">{page}</a>
-    <a data-sveltekit-preload-data="click" href="/?page={page + 1}&type={type}" class="bg-orange-300 font-bold rounded-lg px-4 hover:cursor-pointer">{page + 1}</a>
-    <a data-sveltekit-preload-data="tap" href="/?page={page + 2}&type={type}" class="bg-orange-300 font-bold rounded-lg px-4 hover:cursor-pointer">{page + 2}</a>
+    <div class="join">
+        {#if page != 1 && page != 2}
+            <a href="#" class="join-item btn btn-md">{page - 2}</a>
+        {/if}
+        {#if page != 1}
+            <a href="#" class="join-item btn btn-md">{page - 1}</a>
+        {/if}
+        <a href="#" class="join-item btn btn-md btn-active">{page}</a>
+        <a href="#" class="join-item btn btn-md">{page + 1}</a>
+        <a href="#" class="join-item btn btn-md">{page + 2}</a>
+    </div>
 </div>
