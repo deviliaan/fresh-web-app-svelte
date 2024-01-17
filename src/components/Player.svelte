a3d27a1545a60196ce37d52a0d95633a524d7b8b
kai00588 <61916927+kai00588@users.noreply.github.com>
2024-01-17 09:12:20 +0000

    Nothigs working

--- src/components/Player.svelte
+++ src/components/Player.svelte
@@ -5,31 +5,50 @@
     import { onMount } from "svelte";
     export let episode,id;
 
+    let isPlaying = false;
+    let selectedQuality = 0
+    const qualityOptions = [0,1,2]    
     $: embed = '';
     $: sources = [];
+    const togglePlay=()=>{
+        isPlaying = !isPlaying;
+        const video = document.getElementById('v-player');
+        if(isPlaying){
+            video.play
+        }else{
+            video.pause()
+        }
+    }
+    function changeQuality(newQuality) {
+        selectedQuality = newQuality;
+        const video = document.getElementById('v-player');
+        const currentPlaybackTime = video.currentTime;
+        video.src = getVideoSource();
+        video.load();
+        video.currentTime = currentPlaybackTime;
+        video.play()
+    }
+    function getVideoSource() {
+        return embed[selectedQuality]
+    }
     onMount(async ()=>{
         $isLoading = true;
         const res = await axios.get(`${$api_end}/vidcdn/watch/${id}-episode-${episode}`)
-        embed = res.data.sources;
+        embed = res.data;
         $isLoading = false;
+        
     })
     
 </script>
 
 <div class="flex justify-center items-center pt-2 flex-col">
-    
     {#if $isLoading}
         <div class="bg-slate-600 w-[80%] h-32">
             
         </div>
     {:else}
         <div class="w-[80%]">
-            <!-- svelte-ignore a11y-media-has-caption -->
-            <video class="video-js" controls preload="auto">
-                {#each embed as video}
-                    <source src="{video.file}" type="video/mp4">
-                {/each}
-            </video>
+            
         </div>
     {/if}
     
